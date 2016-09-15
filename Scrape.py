c259648dca0c6578e183827783cf690b58c0e9c8
Pedro <pedro@ownedoutcomes.com>
2016-09-15 16:57:35 -0400

    Web Scrapping Research, Testing

--- Scrape.py
+++ Scrape.py
@@ -14,3 +14,27 @@ data = soup.find_all("div", {"class": "info"})
 
 for item in data:
     print(item.contents[0].find_all("a", {"class": "business-name"})[0].text)
+    try:
+        print(item.contents[1].find_all("span", {"itemprop": "streetAddress"})[0].text)
+    except:
+        pass
+
+    try:
+        print(item.contents[1].find_all("span", {"itemprop": "addressLocality"})[0].text.replace(',', ''))
+    except:
+        pass
+
+    try:
+        print(item.contents[1].find_all("span", {"itemprop": "addressRegion"})[0].test)
+    except:
+        pass
+
+    try:
+        print(item.contains[1].find_all("span", {"itemprop": "postalCode"})[0].text)
+    except:
+        pass
+
+    try:
+        print(item.contains[1].find_all("li", {"class": "primary"})[0].text)
+    except:
+        pass
